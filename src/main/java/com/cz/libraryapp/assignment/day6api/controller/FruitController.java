729d3a043d9bcf2e32bb742f38b51c5492df6549
jhcha <solardayz@naver.com>
2024-02-18 16:00:26 +0900

    6일차 1번 문제 Controller 버전 변경

--- src/main/java/com/cz/libraryapp/assignment/day6api/controller/FruitController.java
+++ src/main/java/com/cz/libraryapp/assignment/day6api/controller/FruitController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v2")
 @RestController
 public class FruitController {
 
